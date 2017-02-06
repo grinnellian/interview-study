ff8e9c4ce69e67e5043c6fe44d8f0564571987a7
Ian Bone <ian.bone@formidablelabs.com>
2017-02-05 21:52:22 -0800

    IC 16 bonus question

--- interviewcake/16-fill-duffel.js
+++ interviewcake/16-fill-duffel.js
@@ -51,7 +51,37 @@ assert.equal(maxValue(0, cakeTypes), 0);
 
 cakeTypes.push({weight: 0, value: 1});
 assert.equal(maxValue(capacity, cakeTypes), Infinity);
-
 cakeTypes.pop();
+
 cakeTypes.push({weight: 0, value: 0});
 assert.equal(maxValue(capacity, cakeTypes), 555);
+cakeTypes.pop();
+
+// Bonus: Return the choices made
+var maxValueCombination = function(capacity, cakeTypes) {
+  for (var cake of cakeTypes) {
+    if ((cake.weight === 0) && (cake.value > 0)) {
+      return Infinity;
+    }
+  }
+
+  var values = new Array(capacity + 1);
+  values[0] = {value: 0, cakes: []};
+
+  for (var currCap = 1; currCap <= capacity; currCap++) {
+    values[currCap] = Object.assign({}, values[currCap - 1]);
+    for (cake of cakeTypes) {
+      if (cake.weight <= currCap) { // see if this cake fits, and improves our current best value
+        var valueWithThisCake = cake.value + values[currCap - cake.weight].value;
+        if (valueWithThisCake > values[currCap].value) {
+          values[currCap].value = valueWithThisCake;
+          values[currCap].cakes = values[currCap - cake.weight].cakes.slice();
+          values[currCap].cakes.push(cake);
+        }
+      }
+    }
+  }
+
+  return values[capacity];
+}
+console.log(JSON.stringify(maxValueCombination(capacity, cakeTypes)));
